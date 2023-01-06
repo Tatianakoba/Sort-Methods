7087d3ffdbef7dd8074adabb5d4cbba84a108e38
TatianaKoba <lila-lilu@mail.ru>
2023-01-07 01:43:18 +0300

    07.01.23 Sort (1-5)

--- Program.cs
+++ Program.cs
@@ -1,15 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Случайная сортировка Bogosort
+using System;
 
-namespace Sort
+class Program
 {
-    internal class Program
+    //метод для проверки упорядоченности массива
+    static bool IsSorted(int[] a)
     {
-        static void Main(string[] args)
+        for (int i = 0; i < a.Length - 1; i++)
         {
+            if (a[i] > a[i + 1])
+                return false;
         }
+
+        return true;
+    }
+
+    //перемешивание элементов массива
+    static int[] RandomPermutation(int[] a)
+    {
+        Random random = new Random();
+        var n = a.Length;
+        while (n > 1)
+        {
+            n--;
+            var i = random.Next(n + 1);
+            var temp = a[i];
+            a[i] = a[n];
+            a[n] = temp;
+        }
+
+        return a;
+    }
+
+    //случайная сортировка
+    static int[] BogoSort(int[] a)
+    {
+        while (!IsSorted(a))
+        {
+            a = RandomPermutation(a);
+        }
+
+        return a;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Случайная сортировка");
+        Console.Write("Введите элементы массива: ");
+        var parts = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+        var array = new int[parts.Length];
+        for (int i = 0; i < parts.Length; i++)
+        {
+            array[i] = Convert.ToInt32(parts[i]);
+        }
+
+        Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", BogoSort(array)));
+
+        Console.ReadLine();
+        Console.ReadKey();
     }
 }
